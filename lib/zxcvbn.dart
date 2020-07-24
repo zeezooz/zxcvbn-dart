4faa6b8b8f1f684c0c0c45218a6afa66688eefa4
Michael Marner <michael@20papercups.net>
2020-07-24 14:03:56 +0930

    Migrate main to dart

---
+++ lib/zxcvbn.dart
@@ -0,0 +1,30 @@
+import 'package:zxcvbn/src/match.dart';
+import 'package:zxcvbn/src/result.dart';
+
+import './src/feedback.dart';
+import './src/matching.dart';
+import './src/scoring.dart';
+import './src/time_estimates.dart';
+
+class Zxcvbn {
+  evaluate(String password, {List<String> userInputs = const []}) {
+    final start = _time;
+    List<String> sanitized_inputs =
+        userInputs.map((input) => input.toLowerCase()).toList();
+
+    matching.set_user_input_dictionary(sanitized_inputs);
+    final List<PasswordMatch> matches = matching.omnimatch(password);
+    final Result result =
+        scoring.most_guessable_match_sequence(password, matches);
+    final calc_time = _time - start;
+
+    final attack_times = time_estimates.estimate_attack_times(result.guesses);
+    attack_times.forEach((prop, val) {
+      result[prop] = val;
+    });
+    result.feedback = feedback.get_feedback(result.score, result.sequence);
+    return result;
+  }
+
+  int get _time => DateTime.now().millisecondsSinceEpoch;
+}
---
+++ test/zxcvbn_test.dart
@@ -0,0 +1,34 @@
+import 'package:test/test.dart';
+import 'package:zxcvbn/src/result.dart';
+import 'package:zxcvbn/zxcvbn.dart';
+
+void main() {
+  group(Zxcvbn, () {
+    final zxcvbn = Zxcvbn();
+    test('zxcvbn', () {
+      Result result = zxcvbn.evaluate('zxcvbn');
+      expect(result.score.round(), 0);
+      expect(double.parse(result.guesses_log10.toStringAsFixed(5)), 1.76343);
+    });
+    test('qwer43@!', () {
+      final result = zxcvbn.evaluate('qwer43@!');
+      expect(result.score.round(), 2);
+      expect(double.parse(result.guesses_log10.toStringAsFixed(5)), 7.3033);
+    });
+    test('Tr0ub4dour&3', () {
+      final result = zxcvbn.evaluate('Tr0ub4dour&3');
+      expect(result.score.round(), 2);
+      expect(double.parse(result.guesses_log10.toStringAsFixed(5)), 7.28008);
+    });
+    test('correcthorsebatterystaple', () {
+      final result = zxcvbn.evaluate('correcthorsebatterystaple');
+      expect(result.score.round(), 4);
+      expect(double.parse(result.guesses_log10.toStringAsFixed(5)), 14.43696);
+    });
+    test('p@ssword', () {
+      final result = zxcvbn.evaluate('p@ssword');
+      expect(result.score.round(), 0);
+      expect(double.parse(result.guesses_log10.toStringAsFixed(5)), 0.69897);
+    });
+  });
+}
